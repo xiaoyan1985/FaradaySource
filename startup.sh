453caf2ce191182b466f1076078c23d575f5fbab
MarkZhang <Mark_Zhang@email.lingan.com.cn>
2018-05-30 14:11:57 +0800

    20180530-011

--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cp -f /root/faraday-dev/server.ini /root/.faraday/config
+cp -f /root/faraday-dev2/server.ini /root/.faraday/config
 couchdb -b
 #./faraday-server.py
 python2 ./faraday-server.py --start
