283f938cf787dd444f949d1f649e9ac0c66aa37b
MarkZhang <Mark_Zhang@email.lingan.com.cn>
2018-05-31 09:21:26 +0800

    20180531-001

--- startup.sh
+++ startup.sh
@@ -4,6 +4,6 @@ chmod 777 ../.faraday/config/server.ini
 cp -f ./config/default.xml ../.faraday/config/config.xml
 chmod 777 ../.faraday/config/config.xml
 couchdb -b
-#./faraday-server.py
+./faraday-server.py
 python2 ./faraday-server.py --start
 python2 ./faraday.py
